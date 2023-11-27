8eda345330af8d29d74333e9f17aa8526493194a
battigfernanda <99561152+battigfernanda@users.noreply.github.com>
2023-11-27 10:32:56 -0500

    Added search bar for filter in list view for sessions

--- ZenZoneProject/Views/MeditationSessionListView.swift
+++ ZenZoneProject/Views/MeditationSessionListView.swift
@@ -9,37 +9,63 @@ import SwiftUI
 
 struct MeditationSessionListView: View {
     @State private var sessions: [MeditationSession] = []
+    @State private var searchText = ""
 
     var body: some View {
         NavigationView {
-            List(sessions) { session in
-                NavigationLink(destination: MeditationSessionDetailView(session: session)) {
-                    HStack {
-                        Image(session.imageName) // Display the session's image
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 50, height: 50)
-                            .cornerRadius(8)
+            VStack {
+                // Search bar for filtering
+                TextField("Search by category or title", text: $searchText)
+                    .padding(7)
+                    .padding(.horizontal, 25)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .padding(.horizontal, 10)
 
-                        VStack(alignment: .leading) {
-                            Text(session.title)
-                                .fontWeight(.bold)
-                            
-                        
-                            Text("Category: \(session.category)")
-                                .font(.caption)
-                                .foregroundColor(.blue)
+                // List of sessions
+                List(filteredSessions) { session in
+                    NavigationLink(destination: MeditationSessionDetailView(session: session)) {
+                        HStack {
+                            Image(session.imageName) // Displaying session's image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(8)
+
+                            VStack(alignment: .leading) {
+                                Text(session.title)
+                                    .fontWeight(.bold)
+                                
+                                Text("Category: \(session.category)")
+                                    .font(.caption)
+                                    .foregroundColor(.blue)
+                            }
                         }
                     }
                 }
+                .navigationTitle("Meditation Sessions")
             }
-            .navigationTitle("Meditation Sessions")
             .onAppear {
                 // Load sessions from the MeditationDataLoader
                 sessions = MeditationDataLoader.loadMeditationSessions()
             }
         }
     }
+
+    // Filter sessions based on the search text
+    private var filteredSessions: [MeditationSession] {
+        if searchText.isEmpty {
+            return sessions
+        } else {
+            return sessions.filter { session in
+                session.title.localizedCaseInsensitiveContains(searchText) ||
+                session.category.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
+
+    
+
 }
 
 struct MeditationSessionListView_Previews: PreviewProvider {
@@ -47,3 +73,6 @@ struct MeditationSessionListView_Previews: PreviewProvider {
         MeditationSessionListView()
     }
 }
+
+
+
